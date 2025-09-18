831c2a86d01b5241d34df45989602d7a3a1f5f23
Milad-alsabbagh13 <alsabbaghmilad@gmail.com>
2025-09-17 21:51:33 -0400

    add votes table in models

--- app/database.py
+++ app/database.py
@@ -2,10 +2,11 @@ from pydantic import BaseModel
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import  sessionmaker
+from .config import settings
 
 
 
-postgres_url = "postgresql://postgres:admin@localhost/fastapi"
+postgres_url = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 engine = create_engine(postgres_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- app/models.py
+++ app/models.py
@@ -21,4 +21,10 @@ class User(Base):
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default='now()', nullable=False)
 
+class Votes(Base):
+    __tablename__="votes"
+    user_id=Column(Integer,ForeignKey("users.id",ondelete="CASCADE"),primary_key=True)
+    post_id=Column(Integer,ForeignKey("posts.id",ondelete="CASCADE"),primary_key=True)
+
+
 
